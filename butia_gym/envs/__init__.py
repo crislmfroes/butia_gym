dff9a38a4242fedddf64c69826238306193bbdbd
crislmfroes <cris.lima.froes@gmail.com>
2022-03-18 15:55:52 -0300

    Update

--- butia_gym/envs/__init__.py
+++ butia_gym/envs/__init__.py
@@ -1,18 +1,1 @@
-from butia_gym.envs.manipulation import DoRISPickAndPlaceEnv
-from gym.envs.registration import (
-    registry,
-    register,
-)
-
-for reward_type in ["sparse", "dense"]:
-    suffix = "Dense" if reward_type == "dense" else ""
-    kwargs = {
-        "reward_type": reward_type,
-    }
-
-    register(
-        id=f"DoRISPickAndPlace{suffix}-v1",
-        entry_point="butia_gym.envs.manipulation:DoRISPickAndPlaceEnv",
-        kwargs=kwargs,
-        max_episode_steps=50,
-    )
+import butia_gym.envs.manipulation
