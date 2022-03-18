258d4326ca900d883673079b6cbc740d93b0ec1a
crislmfroes <cris.lima.froes@gmail.com>
2022-03-18 20:15:27 -0300

    Update

--- butia_gym/envs/manipulation/pick_and_place_env.py
+++ butia_gym/envs/manipulation/pick_and_place_env.py
@@ -8,8 +8,8 @@ import time
 class DoRISPickAndPlaceEnv(RobotTaskEnv):
     def __init__(self, render: bool = False, reward_type: str = "sparse", **kwargs):
         self.sim = PyBullet(render=render)
-        self.robot = DoRISRobot(sim)
-        self.task = DoRISPickAndPlaceTask(sim, reward_type=reward_type, get_ee_position=robot.get_ee_position)
+        self.robot = DoRISRobot(self.sim)
+        self.task = DoRISPickAndPlaceTask(self.sim, reward_type=reward_type, get_ee_position=self.robot.get_ee_position)
         RobotTaskEnv.__init__(self)
         '''self.observation_space['observation'].low = -50*np.ones(shape=self.observation_space['observation'].shape)
         self.observation_space['observation'].high = 50*np.ones(shape=self.observation_space['observation'].shape)
