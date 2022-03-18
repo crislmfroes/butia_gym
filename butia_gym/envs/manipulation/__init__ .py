d515fb9e23b2010de7198ce0b398dcc3744e0aa0
crislmfroes <cris.lima.froes@gmail.com>
2022-03-18 16:38:18 -0300

    Update

--- butia_gym/envs/manipulation/__init__ .py
+++ butia_gym/envs/manipulation/__init__ .py
@@ -1,28 +1,15 @@
 import gym
+from ray.tune.registry import register_env
+
+def ray_env_creator(env_name):
+     def env_creator(env_config):
+          return gym.make(env_name, *env_config)
+     return env_creator
 
 gym.envs.register(
      id='DoRISPickAndPlace-v1',
-     entry_point='butia_gym.envs.manipulation.pick_and_place_env:DoRISPickAndPlaceEnv',
+     entry_point='manipulation_drl.pick_and_place_env:DoRISPickAndPlaceEnv',
      max_episode_steps=50,
 )
 
-#register_env('DoRISPickAndPlace-v1', ray_env_creator('DoRISPickAndPlace-v1'))
-
-
-gym.envs.register(
-     id='DoRISPickAndPlaceShaped-v1',
-     entry_point='butia_gym.envs.manipulation.pick_and_place_env:DoRISPickAndPlaceEnv',
-     max_episode_steps=50,
-     kwargs=dict(
-          reward_type="shaped",
-     )
-)
-
-gym.envs.register(
-     id='DoRISPickAndPlaceDense-v1',
-     entry_point='butia_gym.envs.manipulation.pick_and_place_env:DoRISPickAndPlaceEnv',
-     max_episode_steps=50,
-     kwargs=dict(
-          reward_type="dense",
-     )
-)
+register_env('DoRISPickAndPlace-v1', ray_env_creator('DoRISPickAndPlace-v1'))
