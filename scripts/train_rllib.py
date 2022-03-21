db87afc2c644894b490412be07edcecf864c402e
crislmfroes <cris.lima.froes@gmail.com>
2022-03-20 23:35:01 -0300

    Update

--- scripts/train_rllib.py
+++ scripts/train_rllib.py
@@ -82,12 +82,14 @@ if __name__ == '__main__':
     config['num_gpus'] = 1
     config['num_workers'] = 1
     config['horizon'] = 1000
-    config['model']['dim'] = 48
+    #config['model']['dim'] = 48
     #config['disable_env_checking'] = True
     #config['evaluation_config']['env_config']['render'] = True
     #config['env_config']['reward_threshold'] = 5.0
     #config['env_config']['render'] = True
     config['env_config']['renders'] = True
+    config['env_config']['width'] = 42
+    config['env_config']['height'] = 42
     #config['env_config']['HER_RANDOM'] = True
     #config['env_config']['HER_OPT'] = True
     #config['env_config']['clip_obs'] = True
