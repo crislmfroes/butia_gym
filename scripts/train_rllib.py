d20038a8c41202f968e89892cbcecba3a15bd9c2
crislmfroes <cris.lima.froes@gmail.com>
2022-03-21 22:11:42 -0300

    Update

--- scripts/train_rllib.py
+++ scripts/train_rllib.py
@@ -79,10 +79,10 @@ if __name__ == '__main__':
     #tune.register_env(env_name, lambda cfg: gym.make(env_name))
     config = sac.DEFAULT_CONFIG.copy()
     config['framework'] = 'torch'
+    config['num_gpus'] = 1
+    config['num_workers'] = 7
     #config['num_gpus'] = 1
-    config['num_workers'] = 1
-    #config['num_gpus'] = 1
-    config['num_gpus_per_worker'] = 1
+    #config['num_gpus_per_worker'] = 1
     #config['num_gpus_per_worker'] = 1
     #config['num_gpus_per_trial'] = 1
     #config['horizon'] = 8
