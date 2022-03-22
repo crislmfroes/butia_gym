976f7bb33934f213c08df8d3fc3aee96393ceb5e
crislmfroes <cris.lima.froes@gmail.com>
2022-03-22 01:34:02 -0300

    Update

--- scripts/train_rllib.py
+++ scripts/train_rllib.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     config = sac.DEFAULT_CONFIG.copy()
     config['framework'] = 'torch'
     config['num_gpus'] = 1.0/8.0
-    config['num_workers'] = 7
+    config['num_workers'] = 1
     config['num_gpus_per_worker'] = 1.0/8.0
     #config['num_gpus'] = 1
     #config['num_gpus_per_worker'] = 1
