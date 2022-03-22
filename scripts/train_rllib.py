10b0943c8149ccc29394622ceb352ba51c0f059f
crislmfroes <cris.lima.froes@gmail.com>
2022-03-21 22:03:38 -0300

    Update

--- scripts/train_rllib.py
+++ scripts/train_rllib.py
@@ -3,7 +3,7 @@ import torch
 #import butia_gym.envs.manipulation
 #from butia_gym.envs.manipulation.pick_and_place_env import DoRISPickAndPlaceEnv
 #from butia_gym.envs.manipulation.pick_and_place_task import DoRISPickAndPlaceTask
-from ray.rllib.agents.callbacks import DefaultCallbacks, MultiCallbacks
+from ray.rllib.agents.callbacks import DefaultCallbacks#, MultiCallbacks
 from ray.rllib.agents import sac, es, dreamer
 from ray.rllib import *
 from ray.tune.integration.wandb import WandbLoggerCallback
