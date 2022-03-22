b7434988198c66394dfe66933b6ae4d377c65170
crislmfroes <cris.lima.froes@gmail.com>
2022-03-22 01:46:36 -0300

    Update

--- butia_gym/envs/manipulation/visual_grasp_env.py
+++ butia_gym/envs/manipulation/visual_grasp_env.py
@@ -4,6 +4,6 @@ from pybullet_envs.bullet.kuka_diverse_object_gym_env import KukaDiverseObjectEn
 from gym import spaces
 
 class DoRISDiverseObjectEnv(KukaDiverseObjectEnv):
-    def __init__(self, urdfRoot=..., actionRepeat=80, isEnableSelfCollision=True, renders=False, isDiscrete=False, maxSteps=8, dv=0.06, removeHeightHack=False, blockRandom=0.3, cameraRandom=0, width=48, height=48, numObjects=5, isTest=False):
+    def __init__(self, urdfRoot=..., actionRepeat=80, isEnableSelfCollision=True, renders=False, isDiscrete=False, maxSteps=8, dv=0.06, removeHeightHack=False, blockRandom=0.3, cameraRandom=0, width=48, height=48, numObjects=5, isTest=False, **kwargs):
         super().__init__(urdfRoot, actionRepeat, isEnableSelfCollision, renders, isDiscrete, maxSteps, dv, removeHeightHack, blockRandom, cameraRandom, width, height, numObjects, isTest)
         self.observation_space = spaces.Box(low=0, high=255, shape=self.observation_space.shape, dtype=np.uint8)
--- scripts/train_rllib.py
+++ scripts/train_rllib.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     wandb.login()
     #ray.init(num_cpus=9, num_gpus=1)
     #env_name = 'butia_gym.envs.manipulation.grasp_env.DoRISGraspEnv'
-    env_name = 'pybullet_envs.bullet.KukaDiverseObjectEnv'
+    env_name = 'butia_gym.envs.manipulation.visual_grasp_env.DoRISDiverseObjectEnv'
     #tune.register_env(env_name, lambda cfg: gym.make(env_name))
     config = dreamer.DEFAULT_CONFIG.copy()
     config['framework'] = 'torch'
