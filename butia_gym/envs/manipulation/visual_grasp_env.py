0dd690f051102c33704516425fb65d229bded982
crislmfroes <cris.lima.froes@gmail.com>
2022-03-23 13:31:58 -0300

    Update

--- butia_gym/envs/manipulation/visual_grasp_env.py
+++ butia_gym/envs/manipulation/visual_grasp_env.py
@@ -14,6 +14,7 @@ class DoRISDiverseObjectEnv(KukaDiverseObjectEnv):
     def __init__(self, urdfRoot=pybullet_data.getDataPath(), actionRepeat=80, isEnableSelfCollision=True, renders=False, isDiscrete=False, maxSteps=8, dv=0.06, removeHeightHack=False, blockRandom=0.3, cameraRandom=0, width=48, height=48, numObjects=5, isTest=False, **kwargs):
         super().__init__(urdfRoot, actionRepeat, isEnableSelfCollision, renders, isDiscrete, maxSteps, dv, removeHeightHack, blockRandom, cameraRandom, width, height, numObjects, isTest)
         self.observation_space = spaces.Box(low=0, high=255, shape=self.observation_space.shape, dtype=np.uint8)
+        self._p.setPhysicsEngineParameter(enableFileCaching=False)
 
     def step(self, action):
         return super().step(int(action))
