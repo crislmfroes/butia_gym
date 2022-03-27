9e1be84b0af66242fed31fc65f211ce300dbe5f6
crislmfroes <cris.lima.froes@gmail.com>
2022-03-26 22:07:34 -0300

    Update

--- scripts/collect_experience.sh
+++ scripts/collect_experience.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rllib train --run=PPO --env='butia_gym.envs.manipulation.visual_grasp_env.DoRISDiverseObjectEnv' --config="{\"output\": \"../grasping-out\"}" --stop="{\"timesteps_total\": 1000000}" --framework="torch"
+rllib train --run=PPO --env='butia_gym.envs.manipulation.visual_grasp_env.DoRISDiverseObjectEnv' --config="{\"env_config\": {\"width\": 84, \"height\": 84},  \"output\": \"../grasping-out\"}" --stop="{\"timesteps_total\": 1000000}" --framework="torch"
