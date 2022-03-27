b0df1e3344d9cec2022d3d4306c0656d41f06b4c
crislmfroes <cris.lima.froes@gmail.com>
2022-03-26 22:42:35 -0300

    Update

--- scripts/collect_experience.sh
+++ scripts/collect_experience.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rllib train --run=DDPG --env='butia_gym.envs.manipulation.visual_grasp_env.DoRISDiverseObjectEnv' --config="{\"prioritized_replay\": True, \"num_workers\": 7,  \"num_gpus\": 1, \"env_config\": {\"width\": 84, \"height\": 84},  \"output\": \"../grasping-out\"}" --stop="{\"timesteps_total\": 1000000}" --framework="torch"
+rllib train --run=DDPG --env='butia_gym.envs.manipulation.visual_grasp_env.DoRISDiverseObjectEnv' --config="{\"prioritized_replay\": 1, \"num_workers\": 7,  \"num_gpus\": 1, \"env_config\": {\"width\": 84, \"height\": 84},  \"output\": \"../grasping-out\"}" --stop="{\"timesteps_total\": 1000000}" --framework="torch"
